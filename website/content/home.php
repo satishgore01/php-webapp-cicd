7838778520de4a47402f3a6833fd608ee28c6315
Satish Gore <148797721+satishgore01@users.noreply.github.com>
2024-04-16 07:49:43 +0530

    Update home.php

--- website/content/home.php
+++ website/content/home.php
@@ -1,4 +1,4 @@
-<p>This is <b>home</b> Welcome to Devops-Quest: one week deployment Challanege -- this content is in file -- content/home.php
+<p>This is <b>home</b> Welcome to Devops-Quest: one week deployment Challanege
 </p>
 </p>I🎉 Congratulations on completing the DevOps quest project! 🚀 Your dedication and hard work have paid off, and now you have successfully integrated GitHub, Jenkins, and Docker into a seamless development pipeline. This achievement marks a significant milestone in enhancing efficiency, agility, and quality in software delivery. Your commitment to embracing DevOps principles will undoubtedly drive innovation and success in future projects. Well done on this remarkable accomplishment! 🌟
 </p>
